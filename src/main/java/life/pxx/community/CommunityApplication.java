97f1d36eabb992fc6ecb30c9e98cd8f113f65a2e
maybe <www.721297578@qq.com>
2019-09-27 12:25:20 +0800

    first test

---
+++ src/main/java/life/pxx/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package life.pxx.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author www72
+ */
+@SpringBootApplication
+public class CommunityApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+	
+}
---
+++ src/main/java/life/pxx/community/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package life.pxx.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author pxx
+ * Date 2019/9/27 12:03
+ * @Description
+ */
+@Controller
+public class GreetingController {
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
