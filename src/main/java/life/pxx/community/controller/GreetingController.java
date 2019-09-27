de2b32a1c5345f5a5da31880bb3a494f45bb62b4
maybe <www.721297578@qq.com>
2019-09-27 13:25:10 +0800

    添加主页面 添加页面热部署功能

--- src/main/java/life/pxx/community/controller/GreetingController.java
+++ src/main/java/life/pxx/community/controller/GreetingController.java
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class GreetingController {
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
+	@GetMapping("/")
+	public String greeting() {
+		return "index";
 	}
 }
